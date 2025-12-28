71f4f8a7e2bf09f3cff9eeef51e2b513d914c485
naturavyaherbals-ctrl <naturavyaherbals@gmail.com>
2025-12-29 01:51:20 +0530

    Update testimonials.ts

--- actions/testimonials.ts
+++ actions/testimonials.ts
@@ -1,7 +1,9 @@
 "use server"
-import { supabase } from "@/lib/supabase"
+import { supabaseServer } from "@/lib/supabase"
 
 export async function addTestimonial(formData: FormData) {
+  const supabase = supabaseServer()
+  
   await supabase.from("testimonials").insert([{
     name: formData.get("name"),
     content: formData.get("content"),
