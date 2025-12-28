7db54e644fd6e0c6356eef3e69e67d99cca7656d
naturavyaherbals-ctrl <naturavyaherbals@gmail.com>
2025-12-29 01:49:49 +0530

    Update products.ts

--- actions/products.ts
+++ actions/products.ts
@@ -1,7 +1,9 @@
 "use server"
-import { supabase } from "@/lib/supabase"
+import { supabaseServer } from "@/lib/supabase"
 
 export async function addProduct(formData: FormData) {
+  const supabase = supabaseServer()
+  
   await supabase.from("products").insert([{
     name: formData.get("name"),
     price: Number(formData.get("price")),
