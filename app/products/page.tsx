4a577bd8849ddc320e7f4995cdc4d9a682f0db12
naturavyaherbals-ctrl <naturavyaherbals@gmail.com>
2025-12-29 07:56:22 +0530

    Update page.tsx

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+export const dynamic = "force-dynamic";
+
 import Navbar from "@/components/navbar"
 import Footer from "@/components/footer"
 import WhatsAppButton from "@/components/whatsapp-button"
